2b59bcb9d6e66e337e33b7237f3fc3416b6791d3
Jayson Poon <jaysonpoon@Jaysons-MacBook-Air.local>
2013-06-26 18:22:10 -0700

    add support button functionality

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@ $(function () {
   $('#fb_btn').on('click', function () {
     window.location.href = 'http://facebook.com/doodelsinc';
   });
+  $('#support_btn').click(function(){
+      var token = function(res){
+        var $input = $('<input type=hidden name=stripeToken />').val(res.id);
+        $('form').append($input).submit();
+      };
+
+      StripeCheckout.open({
+        key:         'pk_test_KL3gwhbEY0xY4LVIiBAtfN2V',
+        address:     true,
+        amount:      20,
+        currency:    'usd',
+        name:        'Doodels One Cryon',
+        panelLabel:  'Support',
+        token:       token
+      });
+
+      return false;
+	});
 });
